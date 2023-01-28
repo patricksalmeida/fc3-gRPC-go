5a0b7ce780935c9352b7d02ef9461fc0d3acf320
Patrick Almeida <patricksalmeida@gmail.com>
2023-01-28 14:37:38 -0300

    feat: add grpc server

---
+++ cmd/grpcServer/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"database/sql"
+	"net"
+
+	"github.com/patricksalmeida/fc3-gRPC-go/internal/database"
+	"github.com/patricksalmeida/fc3-gRPC-go/internal/pb"
+	"github.com/patricksalmeida/fc3-gRPC-go/internal/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	categoryDb := database.NewCategory(db)
+	categoryService := service.NewCategoryService(*categoryDb)
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+	reflection.Register(grpcServer)
+
+	lis, err := net.Listen("tcp", ":50051")
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
+}
